74437dd41e4c5fc7371cd01c3fa7b0c124c954ef
Jose Carlos <josecarloscjj@gmail.com>
2023-07-16 12:22:56 -0300

    :sparkles: CRUD User

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,16 @@
 const express = require("express");
-
 const app = express();
-
 const port = 3000;
+const users = require("./controllers/userController");
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.use("/users", users);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
---
+++ backend/services/userService/index.ts
@@ -0,0 +1,79 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const getAllUsers = async () => {
+  try {
+    const users = await prisma.user.findMany();
+    return users;
+  } catch (error) {
+    throw new Error("Error creating user");
+  }
+};
+
+export const createUser = async (reqName: string, reqEmail: string) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: reqName,
+        email: reqEmail,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    throw new Error("Erro ao criar usuário");
+  }
+};
+
+export const getUserById = async (reqId: number) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: reqId,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    throw new Error("User not found");
+  }
+};
+
+export const updateUser = async (
+  reqId: number,
+  reqName: string,
+  reqEmail: string
+) => {
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: reqId,
+      },
+      data: {
+        name: reqName,
+        email: reqEmail,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    throw new Error("Error deleting user");
+  }
+};
+
+export const deleteUser = async (reqId: number) => {
+  try {
+    const user = await getUserById(reqId);
+    if (user) {
+      await prisma.user.delete({
+        where: {
+          id: user.id,
+        },
+      });
+
+      return user;
+    }
+  } catch (error) {
+    throw new Error("Error deleting user");
+  }
+};
